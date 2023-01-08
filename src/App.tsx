66562c14d4cc8c9675a18e1f3317f72bf55b7a37
BenLiu <liumanpan1990@hotmail.com>
2023-01-07 22:14:17 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+function App() {
+  return;
+  <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />;
+      <Route path="/store" element={<Store />} />;
+      <Route path="/about" element={<About />} />;
+    </Routes>
+  </Container>;
+}
+
+export default App;
